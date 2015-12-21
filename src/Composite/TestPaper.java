d6d27dc0e6c423aa2c7b708e2828550d667b3ff6
ChenPoJui <rty6612rty@gmail.com>
2015-12-21 23:09:21 +0800

    ver1.02

--- src/Composite/TestPaper.java
+++ src/Composite/TestPaper.java
@@ -1,5 +1,26 @@
 package Composite;
 
-public class TestPaper {
+import java.awt.List;
+import java.util.ArrayList;
 
+public class TestPaper extends Question{
+	
+	public TestPaper(String desc) {
+		super(desc);
+		// TODO Auto-generated constructor stub
+	}
+
+	private ArrayList<Question> test = new ArrayList<Question>();
+	public void print(){
+		for(Question q : test)
+		{
+			q.print();
+		}
+	}
+	public void add(Question q){
+		this.test.add(q);
+	}
+	public void remove(Question q){
+		test.remove(q);
+	}
 }
