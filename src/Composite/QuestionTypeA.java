6e8d394d03f66b68b50ac80afa2d0c54307139fa
ChenPoJui <rty6612rty@gmail.com>
2015-12-21 22:54:07 +0800

    ver1.01

---
+++ src/Composite/QuestionTypeA.java
@@ -0,0 +1,12 @@
+package Composite;
+
+public class QuestionTypeA extends Question{
+
+	public QuestionTypeA(String desc) {
+		super(desc);
+		// TODO Auto-generated constructor stub
+	}
+	public void print(){
+		System.out.println(description+"from type A");
+	}
+}
---
+++ src/Composite/QuestionTypeB.java
@@ -0,0 +1,13 @@
+package Composite;
+
+public class QuestionTypeB extends Question{
+
+	public QuestionTypeB(String desc) {
+		super(desc);
+		// TODO Auto-generated constructor stub
+	}
+	public void print(){
+		System.out.println(description+"question from typeB");
+	}
+
+}
